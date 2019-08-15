4396dacb80de4eaa769af01ab566cea8e6011797
sanjana23 <arora.sanjana23@gmail.com>
2019-08-15 12:01:58 +0200

    Updates

---
+++ precache-manifest.ebaafe85eacb8efde057fccdcfb9e5e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997c07e566b8372126a70b94af36bec8",
+    "url": "/sanjana-react-proj1/index.html"
+  },
+  {
+    "revision": "1d965bd5adc6a213c441",
+    "url": "/sanjana-react-proj1/static/css/main.1c41eed7.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/sanjana-react-proj1/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "1d965bd5adc6a213c441",
+    "url": "/sanjana-react-proj1/static/js/main.9cf1b59a.chunk.js"
+  },
+  {
+    "revision": "5dedb713a5ce04f8cd37",
+    "url": "/sanjana-react-proj1/static/js/runtime~main.84178a86.js"
+  }
+]);
